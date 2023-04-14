a67ab2d00b7db97fb1cc9a68f181ae3e72d641ac
James Ye <james101994@yahoo.com>
2023-04-13 18:46:27 -0700

    update styling

--- client/components/reusable/Card.js
+++ client/components/reusable/Card.js
@@ -12,21 +12,24 @@ const Card = ({ title, imgPath, summary }) => {
           />
         </div>
         <div className="card-details">
-          <h3>{title}</h3>
-          <ul className="movie-sub-details">
-            <li>PG-13 / </li>
-            <li>2 49min / </li>
-            <li>Action, Superhero</li>
-          </ul>
+          <div className="card-details-wrapper">
+            <h3>{title}</h3>
+            <ul className="card-sub-details">
+              <li>PG-13 / </li>
+              <li>2 49min / </li>
+              <li>Action, Superhero</li>
+            </ul>
 
-          <div className="card-details-summary">
-            <h5 className="card-summary">Summary</h5>
+            <div className="card-details-summary">
+              <h5 className="card-summary">Summary</h5>
+            </div>
+            <div className="card-summary-text">{summary}</div>
+            <div className="card-actors">
+              <span>Actor A</span>
+              <span>Actor B</span>
+            </div>
           </div>
-          <div className="card-summary-text">{summary}</div>
-          <div className="card-actors">
-            <span>Actor A</span>
-            <span>Actor B</span>
-          </div>
+
           <div className="card-action-group">
             <button className="card-btn">Learn More</button>
           </div>
